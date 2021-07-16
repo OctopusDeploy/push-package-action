195faaf7b67179ad73e78b21a4474fbea0a3f58a
jbristowe <john.bristowe@octopus.com>
2021-07-16 13:14:30 +1000

    Fixed error results in Actions runner
    * Updated distribution
    * Fixes #2

--- dist/index.js
+++ dist/index.js
@@ -1866,25 +1866,6 @@ exports.get = get;
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -1896,10 +1877,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.pushPackage = void 0;
-const core = __importStar(__nccwpck_require__(186));
-const exec = __importStar(__nccwpck_require__(514));
+const core_1 = __nccwpck_require__(186);
+const exec_1 = __nccwpck_require__(514);
 function getArgs(parameters) {
-    core.info('🔣 Parsing inputs...');
+    core_1.info('🔣 Parsing inputs...');
     const args = ['push'];
     if (parameters.apiKey.length > 0)
         args.push(`--apiKey=${parameters.apiKey}`);
@@ -1915,7 +1896,7 @@ function getArgs(parameters) {
         parameters.overwriteMode !== 'FailIfExists') {
         if (parameters.overwriteMode !== 'OverwriteExisting' &&
             parameters.overwriteMode !== 'IgnoreIfExists') {
-            core.setFailed('The input value, overwrite_mode is invalid; accept values are "FailIfExists", "OverwriteExisting", and "IgnoreIfExists".');
+            core_1.setFailed('The input value, overwrite_mode is invalid; accept values are "FailIfExists", "OverwriteExisting", and "IgnoreIfExists".');
         }
         args.push(`--overwrite-mode=${parameters.overwriteMode}`);
     }
@@ -1954,37 +1935,38 @@ function pushPackage(parameters) {
     return __awaiter(this, void 0, void 0, function* () {
         const args = getArgs(parameters);
         const options = {
-            ignoreReturnCode: true,
             listeners: {
-                errline: (line) => {
-                    core.error(line);
-                },
                 stdline: (line) => {
                     if (line.length <= 0)
                         return;
                     if (line.includes('Octopus Deploy Command Line Tool')) {
                         const version = line.split('version ')[1];
-                        core.info(`🐙 Using Octopus Deploy CLI ${version}...`);
+                        core_1.info(`🐙 Using Octopus Deploy CLI ${version}...`);
                         return;
                     }
                     if (line.includes('Handshaking with Octopus Server')) {
-                        core.info(`🤝 Handshaking with Octopus Deploy`);
+                        core_1.info(`🤝 Handshaking with Octopus Deploy`);
                         return;
                     }
                     if (line.includes('Authenticated as:')) {
-                        core.info(`✅ Authenticated`);
+                        core_1.info(`✅ Authenticated`);
                         return;
                     }
                     if (line === 'Push successful') {
-                        core.info(`🎉 Push successful!`);
+                        core_1.info(`🎉 Push successful!`);
                         return;
                     }
-                    core.info(line);
+                    core_1.info(line);
                 }
             },
             silent: true
         };
-        yield exec.exec('octo', args, options);
+        try {
+            yield exec_1.exec('octo', args, options);
+        }
+        catch (err) {
+            core_1.setFailed(err);
+        }
     });
 }
 exports.pushPackage = pushPackage;
