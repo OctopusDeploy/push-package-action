e6be1edb6c92ad7ba6e06f092ff8835b7d726f4e
jbristowe <john.bristowe@octopus.com>
2021-09-14 12:31:51 +1000

    fix: added boolean defaults for testing

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -4,9 +4,12 @@ tmp.setGracefulCleanup()
 const tmpdir = tmp.dirSync({template: 'push-package-XXXXXX'})
 process.env = Object.assign(process.env, {
   INPUT_API_KEY: process.env['OCTOPUS_APIKEY'],
+  INPUT_DEBUG: false,
+  INPUT_IGNORE_SSL_ERRORS: false,
   INPUT_SERVER: process.env['OCTOPUS_URL'],
   INPUT_PACKAGES: 'test.nupkg',
   INPUT_OVERWRITE_MODE: 'OverwriteExisting',
+  INPUT_USE_DELTA_COMPRESSION: false,
   RUNNER_TEMP: tmpdir.name,
   RUNNER_TOOL_CACHE: tmpdir.name,
   GITHUB_ACTION: '1'
