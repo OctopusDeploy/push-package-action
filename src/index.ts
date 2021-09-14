8804d441351f1d36c8e87b8347e7b84a74f8d8ad
jbristowe <john.bristowe@octopus.com>
2021-09-14 12:32:13 +1000

    refactor: updated reference and usage for setFailed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as core from '@actions/core'
+import {setFailed} from '@actions/core'
 import * as octopus from './push-package'
 import * as inputs from './input-parameters'
 
@@ -6,8 +6,10 @@ async function run(): Promise<void> {
   try {
     const inputParameters = inputs.get()
     await octopus.pushPackage(inputParameters)
-  } catch (error) {
-    core.setFailed(error.message)
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      setFailed(e)
+    }
   }
 }
 
