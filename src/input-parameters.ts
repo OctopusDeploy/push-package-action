e7ce512ae3f1ef6362678273da11b0fc2b39ef38
hnrkndrssn <henrik@henrikandersson.org>
2022-07-12 10:26:41 +1000

    refactor!: remove unused inputs

--- src/input-parameters.ts
+++ src/input-parameters.ts
@@ -1,43 +1,37 @@
 import {getBooleanInput, getMultilineInput, getInput} from '@actions/core'
 
 export interface InputParameters {
+  // required parameters
+  packages: string[]
   apiKey: string
-  configFile: string
-  debug: boolean
-  ignoreSslErrors: boolean
+  server: string
+  space: string
+  // optional parameters
+  debug?: boolean
   logLevel: string
   overwriteMode: string
-  packages: string[]
-  password: string
   proxy: string
   proxyPassword: string
   proxyUsername: string
-  releaseExisting: string
-  server: string
-  space: string
   timeout: string
   useDeltaCompression: boolean
-  username: string
 }
 
-export function get(): InputParameters {
+export function getInputParameters(): InputParameters {
   return {
+    // required parameters
+    packages: getMultilineInput('packages'),
     apiKey: getInput('api_key'),
-    configFile: getInput('config_file'),
+    server: getInput('server'),
+    space: getInput('space'),
+    // optional parameters
     debug: getBooleanInput('debug'),
-    ignoreSslErrors: getBooleanInput('ignore_ssl_errors'),
     logLevel: getInput('log_level'),
     overwriteMode: getInput('overwrite_mode'),
-    packages: getMultilineInput('packages'),
-    password: getInput('password'),
     proxy: getInput('proxy'),
     proxyPassword: getInput('proxy_password'),
     proxyUsername: getInput('proxy_username'),
-    releaseExisting: getInput('release_existing'),
-    server: getInput('server'),
-    space: getInput('space'),
     timeout: getInput('timeout'),
     useDeltaCompression: getBooleanInput('use_delta_compression'),
-    username: getInput('user')
   }
 }
